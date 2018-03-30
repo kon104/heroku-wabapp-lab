785b0b5002f6f127b8fc3556111f9cd2816ed9c6
kon104 <toshiharu@MacBook-2017.local>
2018-03-31 02:16:18 +0900

    added files of the initial app by maven.

---
+++ src/main/java/com/herokuapp/kon104/webapp/App.java
@@ -0,0 +1,13 @@
+package com.herokuapp.kon104.webapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
