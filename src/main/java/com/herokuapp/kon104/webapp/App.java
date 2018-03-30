23e155b583e926726c74ec7527bbd7234de680f9
kon104 <toshiharu@MacBook-2017.local>
2018-03-31 02:38:39 +0900

    growed codes to a webapp

--- src/main/java/com/herokuapp/kon104/webapp/App.java
+++ src/main/java/com/herokuapp/kon104/webapp/App.java
@@ -1,13 +1,27 @@
 package com.herokuapp.kon104.webapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@RestController
+@EnableAutoConfiguration
 public class App 
 {
+    @RequestMapping("/")
+    public String index()
+    {
+        return "Hello SB!";
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
