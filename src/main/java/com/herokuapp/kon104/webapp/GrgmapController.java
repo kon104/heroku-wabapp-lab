63a5372cd9c63be9fc70c341d67b4cd143b02ed8
kon104 <toshiharu@MacBook-2017.local>
2018-04-01 13:20:37 +0900

    updated files

--- src/main/java/com/herokuapp/kon104/webapp/GrgmapController.java
+++ src/main/java/com/herokuapp/kon104/webapp/GrgmapController.java
@@ -1,6 +1,8 @@
 package com.herokuapp.kon104.webapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Garage Map
@@ -9,9 +11,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class GrgmapController
 {
-    @RequestMapping("/grgmap")
-    public String index()
-    {
-        return "grgmap";
-    }
+
+	@Value("${spring.thymeleaf.cache:}")
+	private String springThymeleafCache;
+
+	@Value("${spring.devtools.restart.enabled:}")
+	private String springDevtoolsRestartEnabled;
+
+	@Value("${server.port:}")
+	private String serverPort;
+
+	@RequestMapping("/grgmap")
+	public String index(Model model)
+	{
+		model.addAttribute("stcache", springThymeleafCache);
+		model.addAttribute("devrest", springDevtoolsRestartEnabled);
+		model.addAttribute("sport", serverPort);
+		model.addAttribute("message", "HelloThymeleaf!!");
+		return "grgmap";
+	}
 }
