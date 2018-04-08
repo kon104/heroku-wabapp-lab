1e4e3b6d11b761c9f8bf9e44a2736713a20b1927
kon104 <toshiharu@MacBook-2017.local>
2018-04-08 22:24:40 +0900

    updated a logic

--- src/main/java/com/herokuapp/kon104/webapp/GrgmapController.java
+++ src/main/java/com/herokuapp/kon104/webapp/GrgmapController.java
@@ -1,6 +1,6 @@
 package com.herokuapp.kon104.webapp;
 
-import org.springframework.beans.factory.annotation.Value;
+//	import org.springframework.beans.factory.annotation.Autowired;;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +14,13 @@ public class GrgmapController
 
 	private String gmapApiKey = "AIzaSyBCYBPbwRyLV_urAoagNVlNn2T3BHspQW4";
 
+//	@Autowired
+//	ApplicationFormsService service;
 
 	@RequestMapping("/grgmap")
 	public String index(Model model)
 	{
+//		model.addAttribute("appforms", service.getAppForms());
 		model.addAttribute("lat", 35.47131841901187);
 		model.addAttribute("lng", 139.4283853703149);
 		model.addAttribute("gmapApiKey", gmapApiKey);
