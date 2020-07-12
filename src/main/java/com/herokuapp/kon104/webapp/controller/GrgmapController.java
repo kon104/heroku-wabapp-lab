5b0c9058177a742cad425a2b9fd4d943a765735a
kon104 <toshiharu@MacBook2017.local>
2020-07-12 23:45:16 +0900

    organized api keys

--- src/main/java/com/herokuapp/kon104/webapp/controller/GrgmapController.java
+++ src/main/java/com/herokuapp/kon104/webapp/controller/GrgmapController.java
@@ -1,7 +1,6 @@
 package com.herokuapp.kon104.webapp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,15 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/grgmap")
 public class GrgmapController
 {
-	private String gmapApiKey = "AIzaSyBCYBPbwRyLV_urAoagNVlNn2T3BHspQW4";
-//	private String gmapApiKey = "AIzaSyCErcdJQ5Pc1jP5pKhaKsBMruAM0GE8tnI";
 
 	// {{{ public String index(Model model)
 	@GetMapping("/")
-	public String index(Model model)
+	public String index()
 	{
-		model.addAttribute("gmapApiKey", gmapApiKey);
-
 		return "grgmap/index";
 	}
 	// }}}
